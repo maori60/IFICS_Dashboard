af017f0345d81f4743d6828257899292a6ede0d3
Los_van_van <143655332+maori60@users.noreply.github.com>
2026-09-07 23:48:11 +0200

    feat(users): expose association role catalogue

---
+++ server/api/roles/index.get.ts
@@ -0,0 +1,23 @@
+import { requirePermission } from '../../utils/auth'
+import { PERMISSIONS } from '../../utils/constants'
+import { prisma } from '../../utils/prisma'
+import { success } from '../../utils/api'
+
+export default defineEventHandler(async (event) => {
+  const context = await requirePermission(event, PERMISSIONS.USER_MANAGE)
+
+  const roles = await prisma.role.findMany({
+    where: { associationId: context.associationId },
+    orderBy: [{ isSystemRole: 'desc' }, { name: 'asc' }],
+    select: {
+      id: true,
+      code: true,
+      name: true,
+      description: true,
+      permissions: true,
+      isSystemRole: true,
+    },
+  })
+
+  return success(roles)
+})
