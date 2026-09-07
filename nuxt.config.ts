3ff5274e5772f6bd73dabc01ee398bb2ce3d5c25
Los_van_van <143655332+maori60@users.noreply.github.com>
2026-09-07 23:02:05 +0200

    feat(m1.4): enable Nuxt ESLint module safely

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: ['@nuxt/eslint'],
+  devtools: {
+    enabled: process.env.NODE_ENV !== 'production',
+  },
 })
