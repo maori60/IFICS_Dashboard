9d358b02294ad707d41f14f2b252928995255972
Los_van_van <143655332+maori60@users.noreply.github.com>
2026-09-08 07:51:03 +0200

    feat(ops): add document-expiry scheduler command

---
+++ scripts/run-document-expiry.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+: "${INTERNAL_JOB_TOKEN:?INTERNAL_JOB_TOKEN must be set}"
+BASE_URL=${INTERNAL_BASE_URL:-http://127.0.0.1:3000}
+
+curl --fail --silent --show-error \
+  --request POST \
+  --header "Authorization: Bearer $INTERNAL_JOB_TOKEN" \
+  --header "Content-Type: application/json" \
+  "$BASE_URL/api/internal/jobs/document-expiry"
+printf '\n'
